0c32c02bd8522670a0eb1d2b160aa2f61a4a0f3f
cadeepakChoudhary <60044473+cadeepakChoudhary@users.noreply.github.com>
2020-01-19 00:09:46 +0530

    Initial commit

---
+++ src/Component/About.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+        <div style={{margin: "20px"}}>
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <h1 class="display-3">About Us</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr/>
+            <div>
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-6"><img class="rounded-circle" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"/>
+                    <h1 >CA Deepak Choudhary</h1><em>Proprietor&nbsp;<br/>CA Deepak Choudhary &amp; Associates<br/><br/>+91-999999999 | deepakchoudhary@gmail.com<br/></em></div>
+                      <div class="col-md-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dapibus felis et libero scelerisque, sed aliquet nulla tincidunt. Quisque lorem nisl, semper vitae mattis nec, vestibulum nec risus. Sed ut volutpat tortor, ut sodales diam. Integer at tristique turpis. Integer orci leo, fringilla ac aliquet vitae, condimentum non lorem. Duis velit ex, aliquam vel pellentesque et, dictum vitae mauris. Vestibulum commodo luctus tortor eu ultrices. Fusce commodo enim non eros consectetur maximus. Donec venenatis dolor ut vestibulum fermentum. Aliquam malesuada sit amet nulla id imperdiet. Suspendisse maximus purus et ligula maximus, a luctus ante fringilla.
+      
+In vitae velit risus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus tincidunt venenatis mi quis congue. Cras eu leo nisi. Ut ornare nisi eget sapien dapibus, eu ornare dolor egestas. Donec venenatis neque lacus, nec mollis odio fermentum a. Maecenas at elit sed diam elementum varius mollis vel arcu. Praesent ornare nisl mi, et posuere purus viverra sed. Sed a lacus nec lorem tincidunt condimentum. Nulla eu tellus pulvinar, tristique metus a, sodales nibh. Ut magna nisi, laoreet id lacus non, cursus bibendum tortor. Sed pretium elit erat. In non pretium nulla.</div>
+            </div>
+        </div>
+    </div>
+    </div>
+    );
+}
+
+export default About;
---
+++ src/Component/Serviceoffered.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+function Serviceoffered() {
+    return (
+        <div class="features-boxed">
+            <div class="container">
+                <div class="intro">
+                    <h2 class="text-center">Features </h2>
+                    <p class="text-center">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae.</p>
+                </div>
+                <div class="row justify-content-center features">
+                    <div class="col-sm-6 col-md-5 col-lg-4 item">
+                        <div class="box"><i class="fa fa-map-marker icon"></i>
+                            <h3 class="name">Works everywhere</h3>
+                            <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Learn more »</a></div>
+                    </div>
+                    <div class="col-sm-6 col-md-5 col-lg-4 item">
+                        <div class="box"><i class="fa fa-clock-o icon"></i>
+                            <h3 class="name">Always available</h3>
+                            <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Learn more »</a></div>
+                    </div>
+                    <div class="col-sm-6 col-md-5 col-lg-4 item">
+                        <div class="box"><i class="fa fa-list-alt icon"></i>
+                            <h3 class="name">Customizable </h3>
+                            <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Learn more »</a></div>
+                    </div>
+                    <div class="col-sm-6 col-md-5 col-lg-4 item">
+                        <div class="box"><i class="fa fa-leaf icon"></i>
+                            <h3 class="name">Organic </h3>
+                            <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Learn more »</a></div>
+                    </div>
+                    <div class="col-sm-6 col-md-5 col-lg-4 item">
+                        <div class="box"><i class="fa fa-plane icon"></i>
+                            <h3 class="name">Fast </h3>
+                            <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Learn more »</a></div>
+                    </div>
+                    <div class="col-sm-6 col-md-5 col-lg-4 item">
+                        <div class="box"><i class="fa fa-phone icon"></i>
+                            <h3 class="name">Mobile-first</h3>
+                            <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Learn more »</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Serviceoffered;
