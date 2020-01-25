2f6d0070047a1a44889b0d1e196783b6727ca00e
cadeepakChoudhary <60044473+cadeepakChoudhary@users.noreply.github.com>
2020-01-26 01:03:29 +0530

    Update Section
    Update Section

--- src/Component/Maincover.js
+++ src/Component/Maincover.js
@@ -3,15 +3,15 @@ import React from 'react';
 function Maincover() {
   return (
     <div class="parallax">
-      <div class="container d-flex justify-content-center align-items-center parallax-content" style={{ height: "100vh", position:"absolute"}}>
+      <div class="container col-12 d-flex justify-content-center align-items-center parallax-content" style={{ height: "100vh", position:"absolute"}}>
         <div class="col-12 col-md-10 col-lg-8 d-flex justify-content-center flex-column">
           <div class="caption v-middle text-center">
             <h1 class="cd-headline clip">
               <span class="blc">Having Trouble <br /> </span>
               <span class="cd-words-wrapper">
                 <b class="is-visible">Tax Filing.</b>
-                <b>GST.</b>
-                <b>Business Planning.</b>
+                <b> Growing Business </b>
+                <b>Managing Finances</b>
               </span>
             </h1>
           </div>
